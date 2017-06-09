196cd69ea0427745135c8f99e55739f33c88445b
Lixiaoyuan <1064997559@qq.com>
2017-06-09 15:58:04 +0800

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-
+//cs
 @NgModule({
   declarations: [
     AppComponent,
